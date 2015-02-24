73cafbc8561aab0c59761a4eaba0558dfe0cc706
fridde <f@hehl.se>
2015-02-24 17:56:08 +0100

    Add functions DEGtoDEC and DECtoDMS

--- functions.php
+++ functions.php
@@ -2987,3 +2987,54 @@
 		error_reporting(E_ALL);
 		ini_set('display_errors', 1); 
 	}
+	
+	function DMStoDEC($deg,$min,$sec){
+
+	// Converts DMS ( Degrees / minutes / seconds ) 
+	// to decimal format longitude / latitude
+
+    	return $deg+((($min*60)+($sec))/3600);
+	}
+	
+	function DECtoDMS($dec)
+	{
+		
+		// Converts decimal longitude / latitude to DMS
+		// ( Degrees / minutes / seconds ) 
+		
+		// This is the piece of code which may appear to 
+		// be inefficient, but to avoid issues with floating
+		// point math we extract the integer part and the float
+		// part by using a string function.
+		
+		$vars = explode(".",$dec);
+		$deg = $vars[0];
+		$tempma = "0.".$vars[1];
+		
+		$tempma = $tempma * 3600;
+		$min = floor($tempma / 60);
+		$sec = $tempma - ($min*60);
+		
+		return array("deg"=>$deg,"min"=>$min,"sec"=>$sec);
+	}    
+	
+	function DECtoDMS($dec){
+		
+		// Converts decimal longitude / latitude to DMS
+		// ( Degrees / minutes / seconds ) 
+		
+		// This is the piece of code which may appear to 
+		// be inefficient, but to avoid issues with floating
+		// point math we extract the integer part and the float
+		// part by using a string function.
+		
+		$vars = explode(".",$dec);
+		$deg = $vars[0];
+		$tempma = "0.".$vars[1];
+		
+		$tempma = $tempma * 3600;
+		$min = floor($tempma / 60);
+		$sec = $tempma - ($min*60);
+		
+		return array("deg"=>$deg,"min"=>$min,"sec"=>$sec);
+	}    
