bf3f8f85b2f15607dafbca1846c80ed7b2972a77
fridde <f@hehl.se>
2015-05-20 10:58:42 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -3058,4 +3058,14 @@
 		}
 		
 		return $resultArray;
-	}		
+	}
+	
+	function generateRandomString($length = 10) {
+    		$characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+		$charactersLength = strlen($characters);
+		 $randomString = '';
+    		for ($i = 0; $i < $length; $i++) {
+        		$randomString .= $characters[rand(0, $charactersLength - 1)];
+    		}
+    		return $randomString;
+	}
