a130a5cda411e77709df107b1d9a941ad78ba6f5
fridde <f@hehl.se>
2015-05-25 18:51:03 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -3153,3 +3153,24 @@
 		}
 		return $output;
 	}
+	
+	function array_orderby(){
+		/* Will sort a 2d-array according to own rules
+			Pass the array, followed by the column names and sort flags
+			$sorted = array_orderby($data, 'volume', SORT_DESC, 'edition', SORT_ASC);
+		*/
+		
+		$args = func_get_args();
+		$data = array_shift($args);
+		foreach ($args as $n => $field) {
+			if (is_string($field)) {
+				$tmp = array();
+				foreach ($data as $key => $row)
+                $tmp[$key] = $row[$field];
+				$args[$n] = $tmp;
+			}
+		}
+		$args[] = &$data;
+		call_user_func_array('array_multisort', $args);
+		return array_pop($args);
+	}
